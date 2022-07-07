db3d56c37b57918ff92b8cd6d2ae6541e385797c
Leonardo <leopdor@gmail.com>
2022-07-06 21:26:41 -0300

    Migração de Vermifugos e Antipulgas para Medicamento

--- src/main/resources/scripts/0001_correcao.sql
+++ src/main/resources/scripts/0001_correcao.sql
@@ -8,3 +8,15 @@ delete from usuario where id > 1;
 
 --changeset Leonardo Rocha:03
 ALTER TABLE public.usuario ADD CONSTRAINT usuario_un UNIQUE (sub);
+
+
+--changeset Leonardo Rocha:04
+update medicamento set tipo_medicamento = 'medicamento';
+
+--changeset Leonardo Rocha:05
+INSERT INTO medicamento (data_medicamento, data_proxima, nome, tipo_medicamento, animal)
+select ap.data_anti_pulga , ap.data_proxima , ap.nome , 'antipulga' , ap.animal from anti_pulga ap ;
+
+--changeset Leonardo Rocha:06
+INSERT INTO medicamento (data_medicamento, data_proxima, nome, tipo_medicamento, animal)
+select v.data_vermifugo , v.data_proximo_vermifugo , v.nome , 'vermifugo', v.animal from vermifugo v ;
